33877bfa4315fd627b0aa0eda80c397c360fa2ec
Uhsendin <chilebr99@gmail.com>
2022-11-09 19:49:45 -0500

    Added password gen

---
+++ app.js
@@ -0,0 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+
+const genBtn = document.getElementById("gen-btn");
+const passwordOne = document.getElementById("password-el-1");
+const passwordTwo = document.getElementById("password-el-2");
+
+function genPassword(length) {
+  passwordOne.textContent = "";
+  passwordTwo.textContent = "";
+
+  for (let i = 0; i < length; i++) {
+    let randomChars1 = Math.floor(Math.random() * characters.length);
+    let randomChars2 = Math.floor(Math.random() * characters.length);
+    passwordOne.textContent += characters[randomChars1];
+    passwordTwo.textContent += characters[randomChars2];
+    
+  }
+}
+
+
